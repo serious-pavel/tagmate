ffaf849e82854b3baebcec501a3df1dad4c371ec
serious-pavel <pvmakhnev@gmail.com>
2025-04-03 19:06:27 +0200

    fix adapter to populate the date

--- app/core/adapters.py
+++ app/core/adapters.py
@@ -4,10 +4,10 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def populate_user(self, request, sociallogin, data):
         """Populate user fields from social login data."""
-        user = super().populate_user(request, sociallogin, data)
+        user = sociallogin.user  # Access the User instance
 
-        user.name = data.get('name', '')
-        # user.profile_picture = data.get('picture') or data.get('avatar_url')
-        user.profile_picture = data.get('picture', '')
+        # Populate custom fields
+        user.full_name = sociallogin.account.extra_data.get('name', '')
+        user.profile_picture = sociallogin.account.extra_data.get('picture', '')
 
         return user
