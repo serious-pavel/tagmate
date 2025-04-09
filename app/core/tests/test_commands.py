e3c5e94b1a931377b7552aecf72ae93b06706b3c
serious-pavel <pvmakhnev@gmail.com>
2025-04-09 12:06:05 +0200

    add test for pre creating command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -7,6 +7,11 @@ from django.db.utils import OperationalError
 from django.test import TestCase
 from psycopg2 import OperationalError as Psycopg2Error
 
+import os
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialAccount
+from django.contrib.auth import get_user_model
+
 
 @patch('django.db.utils.ConnectionHandler.__getitem__')
 class ConnectivityCommandTests(TestCase):
@@ -37,3 +42,38 @@ class ConnectivityCommandTests(TestCase):
 
         # Ensure it retried 6 times
         self.assertEqual(mocked_conn.cursor.call_count, 6)
+
+
+class PreCreateCommandTests(TestCase):
+    """
+    Test for pre_create_su command
+    """
+    @patch.dict(os.environ, {
+        "SU_EMAIL": "admin@example.com",
+        "SU_UID": "google-uid-123"
+    })
+    def test_creates_new_superuser_with_social_account(self):
+        """
+        Test that pre-creating a user with social account works
+        """
+        User = get_user_model()
+        email = os.environ["SU_EMAIL"]
+        uid = os.environ["SU_UID"]
+
+        call_command("pre_create_su")
+
+        # Check user created
+        user = User.objects.get(email=email)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+        self.assertFalse(user.has_usable_password())
+
+        # Check social account created
+        social_acc = SocialAccount.objects.get(user=user, provider="google")
+        self.assertEqual(social_acc.uid, uid)
+
+        # Check email address record
+        email_address = EmailAddress.objects.get(user=user, email=email)
+        self.assertTrue(email_address.verified)
+        self.assertTrue(email_address.primary)
