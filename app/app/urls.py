e42f68d782217e67567cf30e3679b48dc7545872
serious-pavel <pvmakhnev@gmail.com>
2025-04-11 16:16:18 +0200

    limit urls only by essential views

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from allauth.socialaccount.providers.google import views as google_views
+from allauth.account.views import LogoutView
+from allauth.socialaccount.views import (
+    ConnectionsView,
+    LoginCancelledView,
+    LoginErrorView,
+    SignupView
+)
+
+account_urlpatterns = [
+    path('google/login/', google_views.oauth2_login, name='google_login'),
+    path('google/login/callback/', google_views.oauth2_callback, name='google_callback'),
+    path('logout/', LogoutView.as_view(), name='account_logout'),
+    path('social/connections/', ConnectionsView.as_view(), name='social_connections'),
+    path('social/signup/', SignupView.as_view(), name='social_signup'),
+    path('social/login/cancelled/', LoginCancelledView.as_view(),
+         name='social_login_cancelled'),
+    path('social/login/error/', LoginErrorView.as_view(), name='social_login_error'),
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),  # Django-Allauth URLs
+    path('accounts/', include(account_urlpatterns)),
 ]
