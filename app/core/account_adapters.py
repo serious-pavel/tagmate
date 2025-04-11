9486411c08fdadba897f618a17703dde261830e3
serious-pavel <pvmakhnev@gmail.com>
2025-04-11 12:15:29 +0200

    block local logins

--- app/core/account_adapters.py
+++ app/core/account_adapters.py
@@ -4,3 +4,6 @@ from allauth.account.adapter import DefaultAccountAdapter
 class NoSignUpAccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
         return False
+
+    def authenticate(self, request, **credentials):
+        return None
