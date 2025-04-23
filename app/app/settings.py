32fa9950b24701cb31be6e46d7ffcbe564274109
serious-pavel <pvmakhnev@gmail.com>
2025-04-23 13:23:01 +0200

    add Post basic model

--- app/app/settings.py
+++ app/app/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',  # Enable Google authentication
+    'posts',
 ]
 
 MIDDLEWARE = [
--- app/posts/models.py
+++ app/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
