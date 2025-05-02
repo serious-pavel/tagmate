a1167402faccd14383fcb932a2b7310c2bd40480
serious-pavel <pvmakhnev@gmail.com>
2025-05-02 16:50:08 +0200

    removed redundant import

--- app/posts/models.py
+++ app/posts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.apps import apps
-import re
 from django.core.validators import RegexValidator
 
 
