ed01e47074142cf9147bdb4514d8c5700dbee624
serious-pavel <pvmakhnev@gmail.com>
2025-05-12 12:12:10 +0200

    added test updated_at on bulk operation

--- app/posts/tests/test_models.py
+++ app/posts/tests/test_models.py
@@ -503,6 +503,16 @@ class TagGroupSignalTests(TestCase):
             delta=self.time_delta
         )
 
+    def test_updated_at_on_bulk_add(self):
+        old_updated_at = self.tag_group1.updated_at
+        time.sleep(self.longer_time_delta)
+        self.tag_group1.tags.set([self.tag1, self.tag2])
+        self.assertNotAlmostEqual(
+            self.tag_group1.updated_at.timestamp(),
+            old_updated_at.timestamp(),
+            delta=self.time_delta
+        )
+
 
 class PostModelTests(TestCase):
     """Tests for Post model"""
