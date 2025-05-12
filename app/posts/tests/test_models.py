2b974e46ebbceb70d2c7ac77eaa18bbad235c6e7
serious-pavel <pvmakhnev@gmail.com>
2025-05-12 11:44:52 +0200

    added test updated on add Tags

--- app/posts/tests/test_models.py
+++ app/posts/tests/test_models.py
@@ -470,6 +470,16 @@ class TagGroupSignalTests(TestCase):
             delta=self.time_delta
         )
 
+    def test_updated_at_on_add_tags(self):
+        old_updated_at = self.tag_group1.updated_at
+        time.sleep(self.longer_time_delta)
+        self.tag_group1.tags.add(self.tag1)
+        self.assertNotAlmostEqual(
+            self.tag_group1.updated_at.timestamp(),
+            old_updated_at.timestamp(),
+            delta=self.time_delta
+        )
+
 
 class PostModelTests(TestCase):
     """Tests for Post model"""
