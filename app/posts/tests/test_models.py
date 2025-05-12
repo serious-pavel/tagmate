eb6811d5a3ede86f595483b9059503d3a3151bbc
serious-pavel <pvmakhnev@gmail.com>
2025-05-12 11:46:14 +0200

    added test updated on remove Tags

--- app/posts/tests/test_models.py
+++ app/posts/tests/test_models.py
@@ -480,6 +480,17 @@ class TagGroupSignalTests(TestCase):
             delta=self.time_delta
         )
 
+    def test_updated_at_on_remove_tags(self):
+        self.tag_group1.tags.add(self.tag1)
+        old_updated_at = self.tag_group1.updated_at
+        time.sleep(self.longer_time_delta)
+        self.tag_group1.tags.remove(self.tag1)
+        self.assertNotAlmostEqual(
+            self.tag_group1.updated_at.timestamp(),
+            old_updated_at.timestamp(),
+            delta=self.time_delta
+        )
+
 
 class PostModelTests(TestCase):
     """Tests for Post model"""
