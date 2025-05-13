a753fd379bf07f526734b8e0a018d1dd41b178e6
serious-pavel <pvmakhnev@gmail.com>
2025-05-13 14:36:32 +0200

    removed old index page

--- app/core/views.py
+++ app/core/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, template_name='index.html')
-
-
 def profile(request):
     return render(request, template_name='core/profile.html')
