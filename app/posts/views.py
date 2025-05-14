0d9b6613f0aa782ab193e6490430da34bd3afdff
serious-pavel <pvmakhnev@gmail.com>
2025-05-14 17:18:31 +0200

    added initial content to the post_editor page

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render  # noqa
+from posts.models import Post
 
 
 def post_editor(request):
-    return render(request, template_name='posts/post_editor.html')
+    latest_post = Post.objects.filter(user=request.user).order_by('-updated_at').first()
+    return render(
+        request,
+        template_name='posts/post_editor.html',
+        context={'current_post': latest_post}
+    )
