96d34f9d1127b1d61f2229a83730bebf2b959805
serious-pavel <pvmakhnev@gmail.com>
2025-05-26 19:51:29 +0200

    added view for creating Posts, moved logic there

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from posts.models import Post, Tag
 
 
@@ -11,13 +11,6 @@ def post_editor(request, pk=None):
     context['current_post'] = current_post
 
     if request.method == 'POST':
-        action = request.POST.get('action')
-        if action == 'create_post':
-            new_post_title = request.POST.get('new_post_title') or 'Untitled Post'
-            current_post = Post(user=request.user, title=new_post_title)
-            current_post.save()
-            return render(request, 'posts/post_editor.html', context)
-
         tag_names_str = request.POST.get('tag_names')
         if tag_names_str:
             tag_ids = []
@@ -49,3 +42,10 @@ def post_editor(request, pk=None):
         template_name='posts/post_editor.html',
         context=context
     )
+
+
+def create_post(request):
+    new_post_title = request.POST.get('new_post_title') or 'Untitled Post'
+    new_post = Post(user=request.user, title=new_post_title)
+    new_post.save()
+    return redirect('post_editor', pk=new_post.id)
