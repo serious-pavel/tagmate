fe6b78de5154042fda04b76499629e23ca56b623
serious-pavel <pvmakhnev@gmail.com>
2025-05-26 18:31:46 +0200

    added url for post

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -3,4 +3,5 @@ from posts import views as posts_views
 
 urlpatterns = [
     path('', posts_views.post_editor, name='index'),
+    path('post/<int:pk>', posts_views.post_editor, name='post_editor')
 ]
