5e18a8492902895c806d2214c2c7780c1c4c2b6b
serious-pavel <pvmakhnev@gmail.com>
2025-05-27 19:13:43 +0200

    added url for Post deleing

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', posts_views.post_editor, name='index'),
     path('post/<int:pk>', posts_views.post_editor, name='post_editor'),
     path('posts/create', posts_views.create_post, name='create_post'),
+    path('posts/delete/<int:pk>', posts_views.delete_post, name='delete_post'),
 ]
