6e4f254f81f35cd093ce908609dd7d0d799eedce
serious-pavel <pvmakhnev@gmail.com>
2025-06-02 13:06:30 +0200

    renamed parameter names for consistency

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -3,8 +3,8 @@ from posts import views as posts_views
 
 urlpatterns = [
     path('', posts_views.post_editor, name='index'),
-    path('post/<int:pk>', posts_views.post_editor, name='post_editor'),
+    path('post/<int:post_pk>', posts_views.post_editor, name='post_editor'),
     path('posts/create', posts_views.create_post, name='create_post'),
-    path('posts/delete/<int:pk>', posts_views.delete_post, name='delete_post'),
-    path('posts/update/<int:pk>', posts_views.update_post, name='update_post'),
+    path('posts/delete/<int:post_pk>', posts_views.delete_post, name='delete_post'),
+    path('posts/update/<int:post_pk>', posts_views.update_post, name='update_post'),
 ]
