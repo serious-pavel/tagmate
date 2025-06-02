6fe14dccd4e0d59b063ce0a13d19ccb93f5440ef
serious-pavel <pvmakhnev@gmail.com>
2025-06-02 17:07:51 +0200

    added tg_editor link

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -4,6 +4,7 @@ from posts import views as posts_views
 urlpatterns = [
     path('', posts_views.post_editor, name='index'),
     path('post/<int:post_pk>', posts_views.post_editor, name='post_editor'),
+    path('tg/<int:tg_pk>', posts_views.post_editor, name='tg_editor'),
     path('post/<int:post_pk>/tg/<int:tg_pk>', posts_views.post_editor,
          name='post_editor_tg'),
     path('posts/create', posts_views.create_post, name='create_post'),
