f18eaef09551e70bbd38b1b7befa26750c5d96a1
serious-pavel <pvmakhnev@gmail.com>
2025-06-06 17:04:16 +0200

    changed logic of POst deletion to use new function

--- app/posts/views.py
+++ app/posts/views.py
@@ -88,11 +88,8 @@ def post_editor(request, post_pk=None, tg_pk=None):
                 return redirect_post_editor(request, current_post.id, tg_pk)
 
             if action == 'delete_post':
-                # Fetching and deleting the Tags that are not used in any other post
-                # TODO seems inefficient, fetches all the PostTag before Counter
-                # TODO add counter for TagGroups (should be 0)
-                counted_tags = Tag.objects.annotate(pt_count=Count('posttag'))
-                counted_tags.filter(posttag__post=current_post, pt_count__lte=1).delete()
+                # Deleting the Tags that are not used in any other Post or ANY TagGroup
+                current_post.clear_tags()
 
                 current_post.delete()
                 messages.success(request, f'Post {current_post.title} deleted')
