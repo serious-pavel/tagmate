bad110aa7c781698f76e4ea3eb0d0058d98644b6
serious-pavel <pvmakhnev@gmail.com>
2025-06-16 17:52:39 +0200

    added copy preview action to general messaging system

--- app/static/js/copy_preview_text.js
+++ app/static/js/copy_preview_text.js
@@ -3,7 +3,10 @@ const el = document.querySelector('.post-preview-text');
 if (!el) return;
 const plainText = el.innerText; // strips html, gets plain text only
 navigator.clipboard.writeText(plainText)
+    .then(() => {
+        showMessage('Copied to clipboard!', 'err');
+    })
     .catch(err => {
-        alert('Error copying text: ' + err);
+        showMessage('Error copying text: ' + err, 'error');
     });
 }
