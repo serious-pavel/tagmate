0b6b8f8703a4fba556ecdaae125468d02273db8b
serious-pavel <pvmakhnev@gmail.com>
2025-06-16 17:40:41 +0200

    added url for update Tags 'api'

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('tg/<int:tg_pk>', posts_views.post_editor, name='tg_editor'),
     path('post/<int:post_pk>/tg/<int:tg_pk>', posts_views.post_editor,
          name='post_tg_editor'),
+    path('posts/api/<int:post_pk>/reorder_tags', posts_views.reorder_tags,
+         name='reorder_tags')
 ]
