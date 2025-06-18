465b29623f640890f3d10217419bb935e9e852f9
serious-pavel <pvmakhnev@gmail.com>
2025-06-18 14:03:23 +0200

    added uid to the profile page

--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from allauth.socialaccount.models import SocialAccount
 
 
 def profile(request):
-    return render(request, template_name='core/profile.html')
+    user = request.user
+    context = {}
+    if user.is_authenticated:
+        social_acc = SocialAccount.objects.filter(
+            provider='google',
+            user_id=user.id,
+        ).first()
+        if social_acc:
+            context.update({'social_acc': social_acc})
+
+    return render(request, template_name='core/profile.html', context=context)
