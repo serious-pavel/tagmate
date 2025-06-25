d97844538914372b6218157a7b46b7c6c6c80cdf
serious-pavel <pvmakhnev@gmail.com>
2025-06-25 22:38:03 +0200

    fixing workflow error with tag reorder test

--- app/posts/tests/test_tag_reorder.py
+++ app/posts/tests/test_tag_reorder.py
@@ -41,7 +41,7 @@ class TestTagReorderUI(StaticLiveServerTestCase):
             chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument(f"--user-data-dir={tmpdirname}")
+            # chrome_options.add_argument(f"--user-data-dir={tmpdirname}")
             chrome_options.binary_location = "/usr/bin/chromium"
             # Specify exact path to chromedriver!
             driver = webdriver.Chrome(
