908e05944f7ccd646cc5e100895c12f52b6b6fc4
serious-pavel <pvmakhnev@gmail.com>
2025-06-25 20:47:26 +0200

    fixing workflow error with tag reorder test

--- app/posts/tests/test_tag_reorder.py
+++ app/posts/tests/test_tag_reorder.py
@@ -40,7 +40,7 @@ class TestTagReorderUI(StaticLiveServerTestCase):
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument(f"--user-data-dir={tmpdirname}")
-            chrome_options.binary_location = "/usr/bin/chromium-browser"
+            chrome_options.binary_location = "/usr/bin/chromium"
             # Specify exact path to chromedriver!
             driver = webdriver.Chrome(
                 service=Service(executable_path="/usr/bin/chromedriver"),
