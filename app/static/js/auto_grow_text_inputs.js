7f33169a3db80d4ba141c2e20ffaffcea3ca6f0a
serious-pavel <pvmakhnev@gmail.com>
2025-07-03 21:48:40 +0200

    added autogrowth for inputs as well

---
+++ app/static/js/auto_grow_text_inputs.js
@@ -0,0 +1,23 @@
+function auto_grow(element) {
+  element.style.height = "5px";
+  element.style.height = (element.scrollHeight) + "px";
+}
+
+// Expand on page load for prefilled content
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('textarea').forEach(auto_grow);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    function autoResizeInput(input) {
+        if (input.tagName !== "INPUT") return;
+        input.style.width = "2ch"; // reset to minimum
+        input.style.width = (input.value.length + 1) + "ch";
+    }
+    document.querySelectorAll('.edit-like-text').forEach(input => {
+        autoResizeInput(input);
+        input.addEventListener('input', function () {
+            autoResizeInput(input);
+        });
+    });
+});
--- app/static/js/auto_grow_textarea.js
+++
@@ -1,9 +0,0 @@
-function auto_grow(element) {
-  element.style.height = "5px";
-  element.style.height = (element.scrollHeight) + "px";
-}
-
-// Expand on page load for prefilled content
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('textarea').forEach(auto_grow);
-});
