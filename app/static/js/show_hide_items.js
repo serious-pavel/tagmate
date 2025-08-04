0a7e96bb51cfc037858e4f1c6d1bd8a212835bdb
serious-pavel <pvmakhnev@gmail.com>
2025-08-04 11:05:32 +0200

    refactored script showing\hiding the item-create subblock, added focus on to the input on click

--- app/static/js/show_hide_items.js
+++ app/static/js/show_hide_items.js
@@ -1,9 +1,22 @@
-function showItemCreate(item)
-{
-    var btn = document.getElementById(item + "-fake-btn");
-    if (btn.style.display === "none") {
-        btn.style.display = "block";
+const DISPLAY_BLOCK = "block";
+const DISPLAY_NONE = "none";
+
+function focusInputForItem(item) {
+    if (item === "post") {
+        const input = document.querySelector('input[name="new_post_title"]');
+        if (input) input.focus();
+    } else if (item === "tg") {
+        const input = document.querySelector('input[name="new_tg_name"]');
+        if (input) input.focus();
+    }
+}
+
+function toggleItemCreateVisibility(item) {
+    const btn = document.getElementById(item + "-fake-btn");
+    if (btn.style.display === DISPLAY_NONE) {
+        btn.style.display = DISPLAY_BLOCK;
     } else {
-        btn.style.display = "none";
+        btn.style.display = DISPLAY_NONE;
+        focusInputForItem(item);
     }
 }
