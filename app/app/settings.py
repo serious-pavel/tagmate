24816ace82ce0595a95cb6dbe0efeef3c4c7eea7
serious-pavel <pvmakhnev@gmail.com>
2025-08-06 13:16:29 +0200

    fixed LOGIN_URL

--- app/app/settings.py
+++ app/app/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 
-LOGIN_URL = '/accounts/google/login/'
+LOGIN_URL = '/accounts/profile/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
