616c7b7a23a31372a3cefb165dea136fa662493c
serious-pavel <pvmakhnev@gmail.com>
2025-08-18 15:42:14 +0200

    removed unused import

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.db import transaction
-from django.apps import apps
 from django.core.validators import RegexValidator
 from django.db.models import Count
 from django.db.models.signals import m2m_changed, pre_delete
