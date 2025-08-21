578e50d3fbdc95a2381a56f948e6f7d85df4d816
serious-pavel <pvmakhnev@gmail.com>
2025-08-21 12:32:52 +0200

    changed an overridden method to hit more possible sources of changes. clean hits only changes through forms.

--- app/posts/fields.py
+++ app/posts/fields.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class StrippedCharField(models.CharField):
-    def clean(self, value, model_instance):
-        value = super().clean(value, model_instance)
+    def pre_save(self, model_instance, add):
+        value = getattr(model_instance, self.attname)
         if isinstance(value, str):
             value = value.strip()
+            setattr(model_instance, self.attname, value)
         return value
