6fcfdc961011f6bb0d1343b7c6538f002b1726e7
serious-pavel <pvmakhnev@gmail.com>
2025-08-21 13:09:16 +0200

    changed Post title field

--- app/posts/models.py
+++ app/posts/models.py
@@ -217,7 +217,7 @@ class Post(TagOperationMixin):
     objects: models.Manager['Post']
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    title = models.CharField(max_length=100)
+    title = StrippedCharField(max_length=100)
     description = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
